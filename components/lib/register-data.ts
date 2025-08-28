04726e7c4812f6a8a321baebb562c9c4e358741d
dsamithmendis <samithmendis.01@gmail.com>
2025-08-29 03:41:52 +0530

    dev: build foundation

---
+++ components/lib/register-data.ts
@@ -0,0 +1,35 @@
+export const cities = [
+  "Colombo",
+  "Negombo",
+  "Kalutara",
+  "Panadura",
+  "Wattala",
+  "Kandy",
+  "Matale",
+  "Nuwara Eliya",
+  "Galle",
+  "Matara",
+  "Hambantota",
+  "Jaffna",
+  "Kilinochchi",
+  "Mannar",
+  "Vavuniya",
+  "Mullaitivu",
+  "Trincomalee",
+  "Batticaloa",
+  "Ampara",
+  "Kurunegala",
+  "Puttalam",
+  "Anuradhapura",
+  "Polonnaruwa",
+  "Badulla",
+  "Monaragala",
+  "Ratnapura",
+  "Kegalle"
+];
+
+export const genders = ["Male", "Female", "Other"];
+
+export const nameLabels = ["Name", "Surname"];
+
+export const passwordLabels = ["Password", "Re-Password"];
